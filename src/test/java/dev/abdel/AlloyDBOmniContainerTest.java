5ae66a13e8dedbf9dae07d53469f513d16625b98
Abdel Sghiouar <abdelfettah@google.com>
2025-02-08 12:58:33 +0100

    add java source files

---
+++ src/test/java/dev/abdel/AlloyDBOmniContainerTest.java
@@ -0,0 +1,42 @@
+package dev.abdel;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.utility.DockerImageName;
+import java.sql.*;
+import static org.assertj.core.api.Assertions.assertThat;
+public class AlloyDBOmniContainerTest {
+
+    @Test
+    void test() throws SQLException {
+        
+
+        try (AlloyDBOmniContainer alloy = new AlloyDBOmniContainer(DockerImageName.parse("google/alloydbomni:15"))) {
+            alloy.start();
+            Connection connection = DriverManager.getConnection(alloy.getJdbcUrl(), alloy.getUsername(), alloy.getPassword());
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT 1");
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            assertThat(resultSet.getInt(1)).isEqualTo(1);
+        }
+
+    }
+
+    @Test
+    void test2() throws SQLException {
+        try (AlloyDBOmniContainer alloy = new AlloyDBOmniContainer(DockerImageName.parse("google/alloydbomni:15"))
+                .withPassword("testpassword")) {
+
+
+            alloy.start();
+            Connection connection = DriverManager.getConnection(alloy.getJdbcUrl(), alloy.getUsername(), alloy.getPassword());
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT 1");
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            assertThat(resultSet.getInt(1)).isEqualTo(1);
+            assertThat(alloy.getPassword()).isEqualTo("testpassword");
+        }
+    }
+
+}
